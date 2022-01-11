e93554b91421d0642c910ccc8bd7df26cb5cbe1d
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-11 15:02:37 +0800

    add content for AddMedicationForm

---
+++ medication-delivery/src/components/AddMedicationForm.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import {generateId, getNewExpirationTime} from '../utilities';
+
+export function AddMedicationForm(props) {
+    const [medication, setMedication] = useState('')
+
+    const handleMedicationChange = ({target}) => {
+        setMedication(target.value)
+    }
+
+    const handleSubmit = event => {
+        if (!medication) {
+            event.preventDefault();
+            return;
+        }
+
+        event.preventDefault();
+        props.addMedication({
+            id: generateId(),
+            text: medication,
+            expiresAt: getNewExpirationTime()
+        })
+        setMedication('');
+    }
+
+    return (
+        <form className='AddMedicationForm' onSubmit={handleSubmit}>
+            <input
+            type='text'
+            aria-label='What do want to order?'
+            placeholder='What do want to order?'
+            value={medication}
+            onChange={handleMedicationChange}
+            />
+            <input type='submit'/>
+        </form>
+    );
+}
