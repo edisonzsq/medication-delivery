c390bc1f693084b92e2eda790dfe83d0b8bcba5a
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-11 14:51:29 +0800

    added content for App.js

--- medication-delivery/src/App.js
+++ medication-delivery/src/App.js
@@ -1,11 +1,47 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import {AddMedicationForm} from './components/AddMedicationForm';
+import {Medication} from './components/Medication';
+import {generateId, getNewExpirationTime} from './utilities';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [medication, setMedication] = useState([
+    {
+      id: generateId(),
+      text: 'Add ne wmedication',
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: 'They will be removed after 10 seconds',
+      expiresAt: getNewExpriationTime(),
+    },
+  ]);
+
+  const addMedication = medication => {
+    setMedication(medication => [medication, ...medication]);
+  }
+
+  const removeMedication = medicationIdToRemove => {
+    setMedication(medication.filter(medicationItem => medicationItem.id !== medicationIdToRemove))
+  }
+
   return (
     <div className="App">
-      
+      <div className="header">
+        <h1>Medication Delivery App</h1>
+      </div>
+      <div className="main">
+        <AddMedicationForm addMedication={addMedication}/>
+        <ul className="medication">
+          {medication.map((medication) => (
+            <Medication key={medication.id} medication={medication} removeMedication={removeMedication}/>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
