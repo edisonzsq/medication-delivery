83629cfabf9f9fe1da36a9c0f122df70211ba596
Sarah Specialist <sarah.specialist@gmail.com>
2022-02-21 10:01:07 +0800

    changed quantity

--- medication-delivery/src/components/Quantity.js
+++ medication-delivery/src/components/Quantity.js
@@ -1,12 +1,17 @@
+import React, {useState} from 'react';
+
 export default function Quantity(props) {
+    const [number, setNumber] = useState("");
+    const [duration, setDuration] = useState("");
+
     return (
         <span>
-            <input type="tel" value={props.medCount} style={{width: 10, padding: 5, marginRight: 10}} ></input>
-            <select style={{padding: 5, marginRight: 10}} defaultValue={'default'}>
+            <input type="tel" value={number} onChange={(e) => setNumber(e.target.value)} style={{width: 10, padding: 5, marginRight: 10}} ></input>
+            <select style={{padding: 5, marginRight: 10}} defaultValue={'default'} onChange={(e) => setDuration(e.target.value)}>
                 <option value="default" hidden="hidden">Duration</option>
-                <option value={props.duration}>weeks</option>
-                <option value={props.duration}>months</option>
-                <option value={props.duration}>years</option>
+                <option value={duration}>weeks</option>
+                <option value={duration}>months</option>
+                <option value={duration}>years</option>
             </select>
         </span>
         
